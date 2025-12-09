af8d47bdd2570358717b0fe30f5733c73ad24799
Eric Thieme-Garmann <erictg96@gmail.com>
2025-12-09 14:39:35 +0100

    add: redirects

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,22 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   async rewrites() {
     return [
       {
         source: '/api/ota',
         destination: 'https://firmware.sensebox.de/mcu-s2/merged-hello.bin',
+      },
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://flash.sensebox.de/:path*',
+        permanent: true,
       },
     ]
   },
